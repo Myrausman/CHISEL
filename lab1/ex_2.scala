4ad5c4909f88ab2f4c635cf4c5a80ae1d2217f31
Maira Usman <99428378+Myrausman@users.noreply.github.com>
2022-09-06 20:04:25 +0500

    Add files via upload

---
+++ lab1/ex_2.scala
@@ -0,0 +1,17 @@
+package lab1
+import chisel3._
+
+class Counter2( ) extends Module{
+    val io = IO (new Bundle {
+        val result = Output ( Bool() )
+    })
+    
+    val count = RegInit (0.U (4.W) )
+    when ( count(3.U) === 1.B ) {
+        count := 0.U
+    }.otherwise {
+        count := count + 1.U
+    }
+    io.result := count(3.U)
+    
+}
